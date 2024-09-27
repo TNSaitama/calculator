fe75c7e16f0ebac825227b3e9d5b53df1654c55e
Wajdi Baccouche <wajdi.baccouche1987@gmail.com>
2024-09-28 00:09:33 +0100

    eval() is a security risk# Please enter the commit message for your changes. Lines starting

--- calc.js
+++ calc.js
@@ -7,5 +7,13 @@ function clearDisplay(){
 display.value ='';
 }
 function calculate(){
-display.value =eval(display.value);// evaluation function is like a calculator: eval(1+2+3) will give 6
+display.value =eval(display.value);
+// eval() : called evaluation function, it works like a calculator : eval(1+2+3) will give 6
+//basically in our calculator it will read the value and give automatic result 
+//if we click on 3 then * then 4 then = in our calculator it will give 12 automatically 
+//
+/*
+IMPORTANT NOTE: eval() allows you to execute a string of code as if it were actual JavaScript code.
+While it is a useful tool, it is a security risk. I would recommend using eval() ONLY for learning purposes and not any actual production code.
+*/
 }
